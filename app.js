6b5d9669765c0fa4dec3f4309f0caee1b8bf548a
Neil Bouiche <neil.bouiche@icloud.com>
2025-01-11 11:04:36 +0100

    post new tour endpoint added

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const fs = require('fs');
-const express = require('express');
+const fs = require('fs')
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+// middleware
+app.use(express.json())
 
+// read data
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+)
 
+// get all tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -16,14 +19,24 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours: tours,
     },
-  });
-});
+  })
+})
 
+// post a new tour
 app.post('/api/v1/tours', (req, res) => {
-  res.send();
-});
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+  tours.push(newTour)
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } })
+    }
+  )
+})
 
-const port = 3000;
+const port = 3000
 app.listen(port, () => {
-  console.log('App running on port ' + port);
-});
+  console.log('App running on port ' + port)
+})
